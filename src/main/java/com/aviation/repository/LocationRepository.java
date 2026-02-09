92ebb964b14a00f945d52dc67ff42833f2fa6b30
eneskoksalmis <koksalmis@gmail.com>
2026-02-09 21:29:26 +0300

    removed unnecessary repository methods

--- src/main/java/com/aviation/repository/LocationRepository.java
+++ src/main/java/com/aviation/repository/LocationRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
-    Optional<Location> findByLocationCode(String locationCode);
     boolean existsByLocationCode(String locationCode);
 }
